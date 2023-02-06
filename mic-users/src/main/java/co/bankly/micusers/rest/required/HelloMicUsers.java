8534734e5e58af80e34a51bae605d4cb76a81450
said hasnaoui <saidhasnaoui.uce@gmail.com>
2023-02-06 10:05:00 +0100

    BC-4 create new projet and add swagger config

---
+++ mic-users/src/main/java/co/bankly/micusers/rest/required/HelloMicUsers.java
@@ -0,0 +1,14 @@
+package co.bankly.micusers.rest.required;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users")
+public class HelloMicUsers {
+    @RequestMapping(method = RequestMethod.GET, value = "/")
+    public String hello(){
+        return "micro service users works!!";
+    }
+}
---
+++ mic-users/src/test/java/co/bankly/micusers/MicUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package co.bankly.micusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicUsersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
