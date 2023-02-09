b878887667e1d8e16a8730cc0aedb35d0cf3c334
Hasnaoui-Said <saidhasnaoui.uce@email.com>
2023-02-09 13:55:41 +0100

    BC-14 create class DTO convertion rest API for user entity

---
+++ mic-users/src/main/java/co/bankly/micusers/rest/converter/UserConverter.java
@@ -0,0 +1,41 @@
+package co.bankly.micusers.rest.converter;
+
+import co.bankly.micusers.models.entity.User;
+import co.bankly.micusers.rest.vo.UserVo;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class UserConverter {
+    public User toBean(UserVo vo) {
+        User bean = new User();
+        bean.setUsername(vo.getUsername());
+        bean.setEmail(vo.getEmail());
+        bean.setPassword(vo.getPassword());
+        if (vo.getFirstName() != null)
+            bean.setFirstName(vo.getFirstName());
+        if (vo.getLastName() != null)
+            bean.setLastName(vo.getLastName());
+        return bean;
+    }
+
+    public UserVo toVo(User bean) {
+        UserVo vo = new UserVo();
+        vo.setUsername(vo.getUsername());
+        vo.setEmail(bean.getEmail());
+        vo.setPassword(bean.getPassword());
+        vo.setFirstName(bean.getFirstName());
+        vo.setLastName(bean.getLastName());
+        vo.setCreateDate(bean.getCreateDate());
+        vo.setUpdateDate(bean.getUpdateDate());
+        vo.setUuid(bean.getUuid().toString());
+        return vo;
+    }
+    public List<UserVo> toVos(List<User> beans) {
+        List<UserVo> vos = new ArrayList<>();
+        beans.forEach(user -> vos.add(this.toVo(user)));
+        return vos;
+    }
+}
